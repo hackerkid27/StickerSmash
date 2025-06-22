b95ad25e45ca4da79f0236a57ec5ffe6832e97b2
= <hackerkid27@users.noreply.github.com>
2025-06-21 19:24:05 -0500

    End of Second Part of the Tutorial

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,7 +6,7 @@ export default function Index() {
     <View style={styles.container}>
       <Text style={styles.text}>Home Screen</Text>
       <Link href="/about" style={styles.button}>
-        Go to About Screen
+        Go to Home Screen
       </Link>
     </View>
   );
